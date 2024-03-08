f48c074cbe89af6e581f64184cb12e69ef543123
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-03-08 18:14:17 -0300

    feat: create home route

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskApi.ModelViews;
+
+namespace TaskApi.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public HomeController()
+        {
+
+        }
+
+        [HttpGet]
+        public HomeView Index()
+        {
+            return new HomeView
+            {
+                Message = "Welcome to tasks api",
+                Documentation = "/swagger"
+            };
+        }
+    }
+}
---
+++ ModelViews/HomeView.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskApi.ModelViews
+{
+    public class HomeView
+    {
+        public required string Message { get; set; }
+        public required string Documentation { get; set; }
+    }
+}
