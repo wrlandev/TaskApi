c22c62c400f83b4fcef0ea39768ed0a519419a57
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-03-08 18:40:29 -0300

    feat: create task controller

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskApi.Data;
+
+namespace TaskApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public TasksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return StatusCode(200, _context.Tasks.ToList());
+        }
+    }
+}
